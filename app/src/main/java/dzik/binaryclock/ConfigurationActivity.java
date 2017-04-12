61be490720c1f3b6d8513531b6babee79729424c
DarnokOk <DarnokOk@gmail.com>
2017-04-12 22:46:17 +0200

    More base for widget

--- app/src/main/java/dzik/binaryclock/ConfigurationActivity.java
+++ app/src/main/java/dzik/binaryclock/ConfigurationActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.appwidget.AppWidgetManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RemoteViews;
 
 public class ConfigurationActivity extends Activity {
@@ -12,6 +13,7 @@ public class ConfigurationActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration);
         setResult(RESULT_CANCELED);
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
@@ -23,9 +25,19 @@ public class ConfigurationActivity extends Activity {
         if (mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
             finish();
         }
+        //TODO: configuration
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onConfigured();
+            }
+        });
+    }
+
+    private void onConfigured() {
         AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
         RemoteViews views = new RemoteViews(this.getPackageName(),
-                R.layout.example_appwidget);
+                R.layout.widget);
         appWidgetManager.updateAppWidget(mAppWidgetId, views);
         Intent resultValue = new Intent();
         resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
--- app/src/main/java/dzik/binaryclock/MyAppWidgetProvider.java
+++ app/src/main/java/dzik/binaryclock/MyAppWidgetProvider.java
@@ -1,13 +1,27 @@
 package dzik.binaryclock;
 
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 import dzik.binaryclock.R;
 
 public class MyAppWidgetProvider extends AppWidgetProvider {
+
     //TODO: don't update too often http://www.androidauthority.com/how-to-create-a-android-widget-726045/
-    
-    RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.activity_main);
-    appWidgetManager.updateAppWidget(currentWidgetId,views);
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int n = appWidgetIds.length;
+        for (int i = 0; i < n; i++) {
+            int appWidgetId = appWidgetIds[i];
+            Intent intent = new Intent(context, ConfigurationActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
 }
