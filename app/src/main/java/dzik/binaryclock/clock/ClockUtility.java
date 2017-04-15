9c31237c8f085e830a394481af6471b3242d4d0f
DarnokOk <DarnokOk@gmail.com>
2017-04-15 21:12:51 +0200

    Clearing
    Change of plans - it will be an app, not a widget, because of how battery consuming it would be. Mistakes were made, but at least I'll have more experience.

--- app/src/main/java/dzik/binaryclock/clock/ClockUtility.java
+++ app/src/main/java/dzik/binaryclock/clock/ClockUtility.java
@@ -4,47 +4,44 @@ import android.util.Log;
 
 import java.util.Calendar;
 
-public class ClockManager {
-    public ClockManager() {
+public final class ClockUtility { //TODO: static class
+    private ClockUtility() {
     }
 
-    public int getHour() {
+    public static int getHour() {
         return Calendar.getInstance().get(Calendar.HOUR);
     }
 
-    public int getMinute() {
+    public static int getMinute() {
         return Calendar.getInstance().get(Calendar.MINUTE);
     }
 
-    public int getSecond() {
+    public static int getSecond() {
         return Calendar.getInstance().get(Calendar.SECOND);
     }
 
-    public boolean[] getBinaryHour() {
+    public static boolean[] getBinaryHour() {
         return getBooleanArray(Integer.toBinaryString(getHour()));
     }
 
-    public boolean[] getBinaryMinute() {
+    public static boolean[] getBinaryMinute() {
         return getBooleanArray(Integer.toBinaryString(getMinute()));
     }
 
-    public boolean[] getBinarySecond() {
+    public static boolean[] getBinarySecond() {
         return getBooleanArray(Integer.toBinaryString(getSecond()));
     }
 
-    private boolean[] getBooleanArray(String binary) {
+    private static boolean[] getBooleanArray(String binary) {
         boolean[] array = new boolean[6];
-        Log.w("XD", binary);
         if(binary.length() < 6) {
             for(int i = 0; i < 6 - binary.length(); i++) {
                 array[i] = false;
-                Log.w("XD", Boolean.toString(array[i]));
             }
         }
         int n = 0;
         for(int i = 6 - binary.length(); i < 6; i++) {
             array[i] = binary.charAt(n) == '1';
-            Log.w("XDD", Boolean.toString(array[i]));
             n++;
         }
         return array;
